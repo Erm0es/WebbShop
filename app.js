9d63e7697030e805a021d0df41c99549f33a19ab
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2024-11-15 16:03:43 +0100

    made the data and started with func to make "cards"

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("hej")
+import products from "./data.js"
+
+function createCards() {
+    const donutCard = document.querySelector(".donut-card")
+    for (let i = 0; i < products.length; i++) {
+        const array = [];
+        let newCard = donutCard.cloneNode(true);
+        donutCard.after(newCard);
+        array.push(newCard);
+    }
+}
+createCards()
+
+products.forEach(async (e) => {
+    console.log(e)
+
+    document.getElementById("title").innerHTML = `${e.title}`
+});        
---
+++ data.js
@@ -0,0 +1,114 @@
+const products = [
+    {
+        id: 1,
+        title: "Caramel Delight", 
+        img: {
+            url: "/Img/caramel-delight.jpg",
+            alt: "Donut with caramel and sprinkles"
+        },
+        price: 15,
+        rating: 4,
+        category: "sweet"
+    },
+    {
+        id: 2,
+        title: "Strawberry Cheesecake",
+        img: {
+            url: "/Img/strawberry-cheesecake.jpg",
+            alt: "Donut with strawberry glace and cheesecake sprinkles"
+        },
+        price: 15,
+        rating: 5,
+        category: "sweet"
+    },
+    {
+        id: 3,
+        title: "The Classic",
+        img: {
+            url: "/Img/classic.jpg",
+            alt: "Classic sugar donut"
+        },
+        price: 10,
+        rating: 4,
+        category: "classic"
+    },
+    {
+        id: 4,
+        title: "Strawbelicious",
+        img: {
+            url: "/Img/strawberry.jpg",
+            alt: "Classic strawberry glaced donut"
+        },
+        price: 10,
+        rating: 3,
+        category: "classic"
+    },
+    {
+        id: 5,
+        title: "Sweet Honey",
+        img: {
+            url: "/Img/honey.jpg",
+            alt: "Honey glaced donut with hard candy bits"
+        },
+        price: 15,
+        rating: 2,
+        category: "sweet"
+    },
+    {
+        id: 6,
+        title: "Chocolate dream",
+        img: {
+            url: "/Img/chocolate.jpg",
+            alt: "Donut with chocolate glace"
+        },
+        price: 10,
+        rating: 5,
+        category: "classic"
+    },
+    {
+        id: 7,
+        title: "Ice Ice Baby",
+        img: {
+            url: "/Img/ice.jpg",
+            alt: "Donut with light blue glace and coconut flakes "
+        },
+        price: 15,
+        rating: 0,
+        category: "special"
+    },
+    {
+        id: 8,
+        title: "Christmasy",
+        img: {
+            url: "/Img/christmas.jpg",
+            alt: "Donut with white glace and red and green sprinkles"
+        },
+        price: 15,
+        rating: 5,
+        category: "special"
+    },
+    {
+        id: 9,
+        title: "Marshmallow Dream",
+        img: {
+            url: "/Img/mmw.jpg",
+            alt: "Donut with sugar glace and marshmallow topping"
+        },
+        price: 15,
+        rating: 3,
+        category: "special"
+    },
+    {
+        id: 10,
+        title: "QueenNut",
+        img: {
+            url: "/Img.cobnut.jpg",
+            alt: "Donut with chocolate glace and cobnut sprinkles"
+        },
+        price: 10,
+        rating: 5,
+        category: "classic"
+    }
+]
+
+export default products;
