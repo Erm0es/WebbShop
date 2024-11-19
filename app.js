7bda1beccf5cace84736c3960b2fe3566337699d
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2024-11-19 16:17:16 +0100

    insert newcard data with product data and error testing

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 import products from "./data.js"
 
-function createCards() {
-    const donutCard = document.querySelector(".donut-card")
-    for (let i = 0; i < products.length; i++) {
-        const array = [];
-        let newCard = donutCard.cloneNode(true);
-        donutCard.after(newCard);
-        array.push(newCard);
+const donutCardTemplate = document.querySelector(".donut-card").content
+const donutContainer = document.querySelector("#donut-container")
+
+function createDonutCards() {
+
+    if(!donutCardTemplate){
+        console.error("template not found: .donut-card");
+        return;
     }
+    if(!donutContainer){
+        console.error("Container not found: #donut-container")
+    }
+    products.forEach(product => {
+
+        //clone donut card template
+        let newCard = donutCardTemplate.cloneNode(true)
+        
+        const imageUrl = product.image?.url || "";
+        const imageAlt = product.image?.alt || "Donut image"
+
+        //Insert newCard data with product data
+        newCard.querySelector(".donut-image").src = imageUrl
+        newCard.querySelector(".donut-image").alt = imageAlt
+        newCard.querySelector(".donut-title").textContent = product.title
+        newCard.querySelector(".donut-price").textContent = product.price
+        newCard.querySelector(".donut-rating-value").textContent = product.rating
+
+        //Insert newCard to donutContainer
+        donutContainer.appendChild(newCard)
+       
+
+    })
 }
-createCards()
-
-products.forEach(async (e) => {
-    console.log(e)
-
-    document.getElementById("title").innerHTML = `${e.title}`
-});        
+createDonutCards()
