fa2f5d7f31f5c465cd8f1a1ca21b35ed11b2ba3f
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2024-11-20 14:38:34 +0100

    started add and sub button functions

--- app.js
+++ app.js
@@ -4,19 +4,18 @@ const donutCardTemplate = document.querySelector(".donut-card").content
 const donutContainer = document.querySelector("#donut-container")
 
 function createDonutCards() {
-
-    if(!donutCardTemplate){
+    if (!donutCardTemplate) {
         console.error("template not found: .donut-card");
         return;
     }
-    if(!donutContainer){
+    if (!donutContainer) {
         console.error("Container not found: #donut-container")
     }
-    products.forEach(product => {
 
+    products.forEach(product => {
         //clone donut card template
         let newCard = donutCardTemplate.cloneNode(true)
-        
+
         const imageUrl = product.image?.url || "";
         const imageAlt = product.image?.alt || "Donut image"
 
@@ -29,8 +28,21 @@ function createDonutCards() {
 
         //Insert newCard to donutContainer
         donutContainer.appendChild(newCard)
-       
-
     })
 }
 createDonutCards()
+
+const subBtn = document.querySelector(".subtract-button")
+const addBtn = document.querySelector(".add-button")
+let input = document.querySelector("#quantity")
+
+addBtn.addEventListener("click", function(){
+    console.log("add")
+    input.value = "add"
+})
+
+subBtn.addEventListener("click", function(){
+    console.log("sub")
+    input.value = "sub"
+
+})
